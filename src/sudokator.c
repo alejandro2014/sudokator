524f75bdafb15469435fd8fa652b83d422287fb1
Alejandro <alejandro@desktop>
2017-02-18 12:57:28 +0100

    Printed first row of the sudoku

--- src/sudokator.c
+++ src/sudokator.c
@@ -12,7 +12,25 @@ int getNumber(int row, int column) {
 
 void printSudoku() {
     printf("+-----------------------------+\n");
-    printf("| x  x  x |         |         |\n");
+    printf("| ");
+    printf("%d", sudoku[0][0]);
+    printf("  ");
+    printf("%d", sudoku[0][1]);
+    printf("  ");
+    printf("%d", sudoku[0][2]);
+    printf(" | ");
+    printf("%d", sudoku[0][3]);
+    printf("  ");
+    printf("%d", sudoku[0][4]);
+    printf("  ");
+    printf("%d", sudoku[0][5]);
+    printf(" | ");
+    printf("%d", sudoku[0][6]);
+    printf("  ");
+    printf("%d", sudoku[0][7]);
+    printf("  ");
+    printf("%d", sudoku[0][8]);
+    printf(" |\n");
     printf("| x  x  x |         |         |\n");
     printf("| x  x  x |         |         |\n");
     printf("+-----------------------------+\n");
