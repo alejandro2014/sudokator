4c73d73588cbe0a8302bc717f5286a1e88105eb9
Alejandro <alejandro@desktop>
2017-02-18 12:50:30 +0100

    Printed empty sudoku

--- src/sudokator.c
+++ src/sudokator.c
@@ -1,6 +1,41 @@
 #include <stdio.h>
 
+int sudoku[9][9];
+
+void setNumber(int row, int column, int value) {
+    sudoku[row][column] = value;
+}
+
+int getNumber(int row, int column) {
+    return sudoku[row][column];
+}
+
+void printSudoku() {
+    printf("+-----------------------------+\n");
+    printf("| x  x  x |         |         |\n");
+    printf("| x  x  x |         |         |\n");
+    printf("| x  x  x |         |         |\n");
+    printf("+-----------------------------+\n");
+    printf("|         |         |         |\n");
+    printf("|         |         |         |\n");
+    printf("|         |         |         |\n");
+    printf("+-----------------------------+\n");
+    printf("|         |         |         |\n");
+    printf("|         |         |         |\n");
+    printf("|         |         |         |\n");
+    printf("+-----------------------------+\n");
+}
+
 int main() {
-    printf("Hello world\n");
+    int i = 0;
+    int j = 0;
+
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
+            setNumber(i, j, 0);
+        }
+    }
+
+    printSudoku();
     return 0;
 }
